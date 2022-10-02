c78a5455ebc8b0ead03a1fed1c3455f5eec450fd
Aleksei Stoyanenko <stoynalex@gmail.com>
2022-10-02 17:36:48 +0500

    feat: add media queries for 1000p resolutions for main page

---
+++ online-zoo/src/assets/scripts/gallery.js
@@ -0,0 +1,26 @@
+const gallery = [
+  {
+    source: "giantPandaGal",
+    petName: "giant pandas",
+    location: "Native to Southwest China",
+    foodType: "herby",
+  },
+  {
+    source: "eaglesGal",
+    petName: "Eagles",
+    location: "Native to South America",
+    foodType: "meet",
+  },
+  {
+    source: "gorrilasGal",
+    petName: "Gorillas",
+    location: "Native to Congo",
+    foodType: "herby",
+  },
+  {
+    source: "gorrilasGal",
+    petName: "Gorillas",
+    location: "Native to Congo",
+    foodType: "herby",
+  },
+];
