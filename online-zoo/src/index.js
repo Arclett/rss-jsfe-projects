7b4e6151570c1fa942e1cf9ddd2f45dfeb91b2b5
Aleksei Stoyanenko <stoynalex@gmail.com>
2022-10-06 21:14:54 +0500

    fix: fixed some queries for donate page

--- online-zoo/src/index.js
+++ online-zoo/src/index.js
@@ -1,6 +1,3 @@
 import "./styles/main.scss";
 import { animation } from "./assets/scripts/animation";
 animation();
-// alert(
-//   "Здравствуй, уважаемый проверяющий. Прошу, по возможности, проверить мою работу 6.10. Буду крайне благодарен."
-// );
