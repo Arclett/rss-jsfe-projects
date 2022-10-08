c625acacf4fa08c4a76dc7e6d0fc0c90d734ffcf
Aleksei Stoyanenko <stoynalex@gmail.com>
2022-10-08 22:42:55 +0500

    fix: fixed logo position in watch srction on main page feat: add popup- window for main page

---
+++ online-zoo/src/assets/scripts/popup.js
@@ -0,0 +1,16 @@
+export const popup = function () {
+  const burgerElement = document.querySelector(".burger");
+  const popupElement = document.querySelector(".popup-window");
+  const overlayElement = document.querySelector(".overlay");
+  const showPopUp = function () {
+    burgerElement.classList.toggle("burger-active");
+    popupElement.classList.toggle("popup-show");
+    overlayElement.classList.toggle("overlay-hidden");
+  };
+  burgerElement.addEventListener("click", function () {
+    showPopUp();
+  });
+  overlayElement.addEventListener("click", function () {
+    showPopUp();
+  });
+};
--- online-zoo/src/index.js
+++ online-zoo/src/index.js
@@ -1,3 +1,5 @@
 import "./styles/main.scss";
 import { animation } from "./assets/scripts/animation";
 animation();
+import { popup } from "./assets/scripts/popup";
+popup();
