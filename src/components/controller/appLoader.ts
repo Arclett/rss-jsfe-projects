d17116f744847559158e15526c39f38683ad348a
Aleksei Stoyanenko <stoynalex@gmail.com>
2022-12-11 20:36:07 +0500

    fix: change link in appLoader refactor: change some styles refactor: change some webpack settings

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
             apiKey: 'becec9dd7075419b876a31bd5be06129', // получите свой ключ https://newsapi.org/
         });
     }
